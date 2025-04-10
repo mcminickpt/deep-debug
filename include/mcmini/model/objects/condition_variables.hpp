mcminickpt/mcmini

coding style fix
spelling fixed

dmtcp/include/mcmini/model/objects/condition_variables.hpp
@@ -15,17 +15,16 @@ struct condition_variable : public model::visible_object_state {
  public:
   /* The four possible states for a condition variable */
   enum state { 
-        cv_uninitialized, 
-        cv_initialized, 
-        cv_waiting, 
-        cv_signalled, 
-        cv_transitional,
-        cv_destroyed
-        };
+    cv_uninitialized, 
+    cv_initialized, 
+    cv_waiting, 
+    cv_signaled, 
+    cv_transitional,
+    cv_destroyed
+  };
 
  private:
-  state current_state = state::cv_uninitialized;
-   
+  state current_state = state::cv_uninitialized; 
   bool hadwaiters;
   mutable unsigned int numRemainingSpuriousWakeups = 0;
   runner_id_t running_thread;
@@ -45,39 +44,38 @@ struct condition_variable : public model::visible_object_state {
   condition_variable(state s, runner_id_t tid, pthread_mutex_t* mutex, int count) 
     : current_state(s), running_thread(tid), associated_mutex(mutex), waiting_count(count){}
 
-    condition_variable(state s, runner_id_t tid, pthread_mutex_t* mutex, int count, 
-      const std::vector<std::pair<runner_id_t, condition_variable_status>>& thread_states) 
-    : current_state(s), running_thread(tid), associated_mutex(mutex), waiting_count(count) {
-      // Initialize the policy according to the states of the threads in waiting queue
-      for (const auto& thread_with_state : thread_states) {
-        if (thread_with_state.second == CV_TRANSITIONAL || 
-            thread_with_state.second == CV_WAITING) {
-          this->policy->add_waiter_with_state(thread_with_state.first,thread_with_state.second);
+  condition_variable(state s, runner_id_t tid, pthread_mutex_t* mutex, int count, 
+    const std::vector<std::pair<runner_id_t, condition_variable_status>>& thread_states) 
+      : current_state(s), running_thread(tid), associated_mutex(mutex), waiting_count(count) {
+        // Initialize the policy according to the states of the threads in waiting queue
+        for (const auto& thread_with_state : thread_states) {
+          if (thread_with_state.second == CV_TRANSITIONAL || thread_with_state.second == CV_WAITING) {
+            this->policy->add_waiter_with_state(thread_with_state.first,thread_with_state.second);
+          }
         }
-      }
-      std::vector<runner_id_t> signaled_threads;
-      for (const auto& thread_with_state : thread_states) {
-        if (thread_with_state.second == CV_SIGNALLED) {
-          signaled_threads.push_back(thread_with_state.first);
+        std::vector<runner_id_t> signaled_threads;
+        for (const auto& thread_with_state : thread_states) {
+          if (thread_with_state.second == CV_SIGNALED) {
+            signaled_threads.push_back(thread_with_state.first);
+          }
         }
-      }
-      if(!signaled_threads.empty()) {
-        // If there are any threads that have been signaled, we should
-        // add them to the wake groups in the policy.
-        this->policy->add_to_wake_groups(signaled_threads);
-      }
-    }
-  
+        if (!signaled_threads.empty()) {
+          // If there are any threads that have been signaled, we should
+          // add them to the wake groups in the policy.
+          this->policy->add_to_wake_groups(signaled_threads);
+        }
+      } 
   // ---- State Observation --- //
   bool operator==(const condition_variable &other) const {
-  return this->current_state == other.current_state;
+    return this->current_state == other.current_state;
   }
   bool operator!=(const condition_variable &other) const {
-  return this->current_state != other.current_state;
+    return this->current_state != other.current_state;
   }
+
   bool is_initialized() const { return this->current_state == cv_initialized ; }
   bool is_waiting() const { return this->current_state == cv_waiting ; }
-  bool is_signalled() const { return this->current_state == cv_signalled ; }
+  bool is_signaled() const { return this->current_state == cv_signaled ; }
   bool is_uninitialized() const { return this->current_state == cv_uninitialized ;}
   bool is_transitional() const { return this->current_state == cv_transitional;}
   bool is_destroyed() const { return this->current_state == cv_destroyed;}
@@ -88,43 +86,44 @@ struct condition_variable : public model::visible_object_state {
     this->associated_mutex = mutex;
   }
 
-
   pthread_mutex_t* get_mutex() const {return this->associated_mutex;}
 
   bool has_waiters() const {return this->policy->has_waiters();}
+
   void remove_waiter(runner_id_t tid) const  {
-        this->policy->wake_thread(tid);
-        // If there are any spurious wake ups allowed,
-        // we always allow the thread to wake up
-        // due to a spurious wake up and decrement the
-        // number of future such wakeups allowed
-        if (this->numRemainingSpuriousWakeups > 0) {
-            this->numRemainingSpuriousWakeups--;
-        }
+    this->policy->wake_thread(tid);
+    // If there are any spurious wake ups allowed,
+    // we always allow the thread to wake up
+    // due to a spurious wake up and decrement the
+    // number of future such wakeups allowed
+    if (this->numRemainingSpuriousWakeups > 0) {
+      this->numRemainingSpuriousWakeups--;
+    }
   }
+
   void add_waiter(runner_id_t tid) {
     this->policy->add_waiter(tid);
   }
+
   bool waiter_can_exit(runner_id_t tid) const{
     return this->numRemainingSpuriousWakeups > 0 || 
     this->policy->thread_can_exit(tid);
   }
 
   void send_signal_message() const{
-        this->policy->receive_signal_message();
+    this->policy->receive_signal_message();
   }
 
   void send_broadcast_message() {
-        this->policy->receive_broadcast_message();
+    this->policy->receive_broadcast_message();
   }
 
   void check_for_lost_wakeup(bool is_signal, int prev_waiting_count) const{
-    if(!is_signal) return;
-
+    if (!is_signal) return;
     // Check if any thread was signaled
     bool any_thread_signaled = false;
     for (auto id : this->policy->return_wait_queue()) {
-      if (this->policy->get_thread_cv_state(id) == CV_SIGNALLED) {
+      if (this->policy->get_thread_cv_state(id) == CV_SIGNALED) {
         any_thread_signaled = true;
         break;
       }
@@ -139,6 +138,7 @@ struct condition_variable : public model::visible_object_state {
   std::unique_ptr<visible_object_state> clone() const override {
     return extensions::make_unique<condition_variable>(*this);
   }
+
   std::string to_string() const override {
     return "condition_variable(state: " + std::to_string(current_state);  
     }

mcminickpt/deep-debug

include/mcmini/model/objects/condition_variables.hpp
@@ -15,17 +15,16 @@ struct condition_variable : public model::visible_object_state {
  public:
   /* The four possible states for a condition variable */
   enum state { 
-        cv_uninitialized, 
-        cv_initialized, 
-        cv_waiting, 
-        cv_signalled, 
-        cv_transitional,
-        cv_destroyed
-        };
+    cv_uninitialized, 
+    cv_initialized, 
+    cv_waiting, 
+    cv_signaled, 
+    cv_transitional,
+    cv_destroyed
+  };
 
  private:
-  state current_state = state::cv_uninitialized;
-   
+  state current_state = state::cv_uninitialized; 
   bool hadwaiters;
   mutable unsigned int numRemainingSpuriousWakeups = 0;
   runner_id_t running_thread;
@@ -45,39 +44,38 @@ struct condition_variable : public model::visible_object_state {
   condition_variable(state s, runner_id_t tid, pthread_mutex_t* mutex, int count) 
     : current_state(s), running_thread(tid), associated_mutex(mutex), waiting_count(count){}
 
-    condition_variable(state s, runner_id_t tid, pthread_mutex_t* mutex, int count, 
-      const std::vector<std::pair<runner_id_t, condition_variable_status>>& thread_states) 
-    : current_state(s), running_thread(tid), associated_mutex(mutex), waiting_count(count) {
-      // Initialize the policy according to the states of the threads in waiting queue
-      for (const auto& thread_with_state : thread_states) {
-        if (thread_with_state.second == CV_TRANSITIONAL || 
-            thread_with_state.second == CV_WAITING) {
-          this->policy->add_waiter_with_state(thread_with_state.first,thread_with_state.second);
+  condition_variable(state s, runner_id_t tid, pthread_mutex_t* mutex, int count, 
+    const std::vector<std::pair<runner_id_t, condition_variable_status>>& thread_states) 
+      : current_state(s), running_thread(tid), associated_mutex(mutex), waiting_count(count) {
+        // Initialize the policy according to the states of the threads in waiting queue
+        for (const auto& thread_with_state : thread_states) {
+          if (thread_with_state.second == CV_PREWAITING || thread_with_state.second == CV_WAITING) {
+            this->policy->add_waiter_with_state(thread_with_state.first,thread_with_state.second);
+          }
         }
-      }
-      std::vector<runner_id_t> signaled_threads;
-      for (const auto& thread_with_state : thread_states) {
-        if (thread_with_state.second == CV_SIGNALLED) {
-          signaled_threads.push_back(thread_with_state.first);
+        std::vector<runner_id_t> signaled_threads;
+        for (const auto& thread_with_state : thread_states) {
+          if (thread_with_state.second == CV_SIGNALED) {
+            signaled_threads.push_back(thread_with_state.first);
+          }
         }
-      }
-      if(!signaled_threads.empty()) {
-        // If there are any threads that have been signaled, we should
-        // add them to the wake groups in the policy.
-        this->policy->add_to_wake_groups(signaled_threads);
-      }
-    }
-  
+        if (!signaled_threads.empty()) {
+          // If there are any threads that have been signaled, we should
+          // add them to the wake groups in the policy.
+          this->policy->add_to_wake_groups(signaled_threads);
+        }
+      } 
   // ---- State Observation --- //
   bool operator==(const condition_variable &other) const {
-  return this->current_state == other.current_state;
+    return this->current_state == other.current_state;
   }
   bool operator!=(const condition_variable &other) const {
-  return this->current_state != other.current_state;
+    return this->current_state != other.current_state;
   }
+
   bool is_initialized() const { return this->current_state == cv_initialized ; }
   bool is_waiting() const { return this->current_state == cv_waiting ; }
-  bool is_signalled() const { return this->current_state == cv_signalled ; }
+  bool is_signaled() const { return this->current_state == cv_signaled ; }
   bool is_uninitialized() const { return this->current_state == cv_uninitialized ;}
   bool is_transitional() const { return this->current_state == cv_transitional;}
   bool is_destroyed() const { return this->current_state == cv_destroyed;}
@@ -88,34 +86,36 @@ struct condition_variable : public model::visible_object_state {
     this->associated_mutex = mutex;
   }
 
-
   pthread_mutex_t* get_mutex() const {return this->associated_mutex;}
 
   bool has_waiters() const {return this->policy->has_waiters();}
+
   void remove_waiter(runner_id_t tid) const  {
-        this->policy->wake_thread(tid);
-        // If there are any spurious wake ups allowed,
-        // we always allow the thread to wake up
-        // due to a spurious wake up and decrement the
-        // number of future such wakeups allowed
-        if (this->numRemainingSpuriousWakeups > 0) {
-            this->numRemainingSpuriousWakeups--;
-        }
+    this->policy->wake_thread(tid);
+    // If there are any spurious wake ups allowed,
+    // we always allow the thread to wake up
+    // due to a spurious wake up and decrement the
+    // number of future such wakeups allowed
+    if (this->numRemainingSpuriousWakeups > 0) {
+      this->numRemainingSpuriousWakeups--;
+    }
   }
+
   void add_waiter(runner_id_t tid) {
     this->policy->add_waiter(tid);
   }
+
   bool waiter_can_exit(runner_id_t tid) const{
     return this->numRemainingSpuriousWakeups > 0 || 
     this->policy->thread_can_exit(tid);
   }
 
   void send_signal_message() const{
-        this->policy->receive_signal_message();
+    this->policy->receive_signal_message();
   }
 
-  void send_broadcast_message() {
-        this->policy->receive_broadcast_message();
+  void send_broadcast_message() const {
+    this->policy->receive_broadcast_message();
   }
 
   void check_for_lost_wakeup(bool is_signal, int prev_waiting_count) {
@@ -123,7 +123,7 @@ struct condition_variable : public model::visible_object_state {
     // Check if any thread was signaled
     bool any_thread_signaled = false;
     for (auto id : this->policy->return_wait_queue()) {
-      if (this->policy->get_thread_cv_state(id) == CV_SIGNALLED) {
+      if (this->policy->get_thread_cv_state(id) == CV_SIGNALED) {
         any_thread_signaled = true;
         break;
       }
@@ -138,6 +138,7 @@ struct condition_variable : public model::visible_object_state {
   std::unique_ptr<visible_object_state> clone() const override {
     return extensions::make_unique<condition_variable>(*this);
   }
+
   std::string to_string() const override {
     return "condition_variable(state: " + std::to_string(current_state);  
     }